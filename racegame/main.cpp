4190e8d293ee6640c5e0b0cfd27188a43adfe178
Matthew Taylor <ecstatic@gmail.com>
2018-09-01 10:57:11 +0100

    Fully implemented line/line collision detection

--- racegame/main.cpp
+++ racegame/main.cpp
@@ -13,6 +13,7 @@ bool* keySpecialStates = new bool[256]();
 GLuint texture[2];
 
 // global variables
+bool debugMode = true;	// draws bounding boxes
 float carLength = 1.0f;
 float carWidth = 0.5f;
 float decelRate = 0.00009f;
@@ -47,55 +48,141 @@ public:
 	float vel_x;	// stores car velocity
 	float vel_y;
 
-// constructor sets car's default position
-Car(float x, float y) {
-pos_x = x;
-pos_y = y;
-rot = 0.0f;
-acceleration = 0.0f;
-}
+	// constructor sets car's default position
+	Car(float x, float y) {
+	pos_x = x;
+	pos_y = y;
+	rot = 0.0f;
+	acceleration = 0.0f;
+	}
 
-std::vector<line> getLines() {
-	std::vector<line> carLines;
+	// returns the corners used for oriented bounding box collision detection
+	std::vector<line> getCarCorners() {
 
-	// left
-	carLines.push_back({ (pos_x - (carWidth / 2)), (pos_y - (carLength / 2)), (pos_x - (carWidth / 2)), (pos_y + (carLength / 2)) });
+		// create corner coords, store in vector
+		float tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y;
+		float tl_xr, tl_yr, tr_xr, tr_yr, bl_xr, bl_yr, br_xr, br_yr;
 
-	// top
-	carLines.push_back({ (pos_x - (carWidth / 2)), (pos_y + (carLength / 2)), (pos_x + (carWidth / 2)), (pos_y + (carLength / 2)) });
+		// set corner values for collision detection
+		tl_x = pos_x - (carWidth / 2);
+		tl_y = pos_y + (carLength / 2);
+		tr_x = pos_x + (carWidth / 2);
+		tr_y = pos_y + (carLength / 2);
+		bl_x = pos_x - (carWidth / 2);
+		bl_y = pos_y - (carLength / 2);
+		br_x = pos_x + (carWidth / 2);
+		br_y = pos_y - (carLength / 2);
 
-	// right
-	carLines.push_back({ (pos_x + (carWidth / 2)), (pos_y + (carLength / 2)), (pos_x + (carWidth / 2)), (pos_y - (carLength / 2)) });
+		// translation to origin
+		tl_x -= pos_x;
+		tl_y -= pos_y;
+		tr_x -= pos_x;
+		tr_y -= pos_y;
+		bl_x -= pos_x;
+		bl_y -= pos_y;
+		br_x -= pos_x;
+		br_y -= pos_y;
+		
+		// rotation
+		tl_xr = (tl_x * cos((rot * (3.14159265359 / 180))) - (tl_y * sin((rot * (3.14159265359 / 180)))));
+		tl_yr = (tl_x * sin((rot * (3.14159265359 / 180))) + (tl_y * cos((rot * (3.14159265359 / 180)))));
 
-	// bottom
-	carLines.push_back({ (pos_x + (carWidth / 2)), (pos_y - (carLength / 2)), (pos_x - (carWidth / 2)), (pos_y - (carLength / 2)) });
+		tr_xr = (tr_x * cos((rot * (3.14159265359 / 180))) - (tr_y * sin((rot * (3.14159265359 / 180)))));
+		tr_yr = (tr_x * sin((rot * (3.14159265359 / 180))) + (tr_y * cos((rot * (3.14159265359 / 180)))));
 
-	return carLines;
-}
+		bl_xr = (bl_x * cos((rot * (3.14159265359 / 180))) - (bl_y * sin((rot * (3.14159265359 / 180)))));
+		bl_yr = (bl_x * sin((rot * (3.14159265359 / 180))) + (bl_y * cos((rot * (3.14159265359 / 180)))));
 
+		br_xr = (br_x * cos((rot * (3.14159265359 / 180))) - (br_y * sin((rot * (3.14159265359 / 180)))));
+		br_yr = (br_x * sin((rot * (3.14159265359 / 180))) + (br_y * cos((rot * (3.14159265359 / 180)))));
+
+		// translate back
+		// translation to origin
+		tl_xr += pos_x;
+		tl_yr += pos_y;
+		tr_xr += pos_x;
+		tr_yr += pos_y;
+		bl_xr += pos_x;
+		bl_yr += pos_y;
+		br_xr += pos_x;
+		br_yr += pos_y;
+
+		std::vector<line> carCorners;
+		// left
+		carCorners.push_back({ tl_xr, tl_yr, bl_xr, bl_yr });
+
+		// top
+		carCorners.push_back({ tl_xr, tl_yr, tr_xr, tr_yr });
+
+		// right
+		carCorners.push_back({ tr_xr, tr_yr, br_xr, br_yr });
+
+		// bottom
+		carCorners.push_back({ br_xr, br_yr, bl_xr, bl_yr });
+
+		return carCorners;
+	}
 };
 
 // initialise objects for game
 Car playerCar = Car(0, 0);
 
+// takes in two vectors containing <lines>, checks to see if any of them intersect
+bool isColliding(std::vector<line> a, std::vector<line> b) {
+	float dist1 = 0;
+	float dist2 = 0;
+
+	// loop through every combination of both vectors
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < b.size(); j++) {
+
+			// calculate distance to point of intersection
+			float dist1 = ((b[j].x2 - b[j].x1) * (a[i].y1 - b[j].y1) - (b[j].y2 - b[j].y1) * (a[i].x1 - b[j].x1)) /
+				((b[j].y2 - b[j].y1) * (a[i].x2 - a[i].x1) - (b[j].x2 - b[j].x1) * (a[i].y2 - a[i].y1));
+
+			float dist2 = ((a[i].x2 - a[i].x1) * (a[i].y1 - b[j].y1) - (a[i].y2 - a[i].y1) * (a[i].x1 - b[j].x1)) /
+				((b[j].y2 - b[j].y1) * (a[i].x2 - a[i].x1) - (b[j].x2 - b[j].x1) * (a[i].y2 - a[i].y1));
+
+			// if dist1 and dist1 are both between 0 and 1, there is a collision
+			if (dist1 >= 0 && dist1 <= 1 && dist2 >= 0 && dist2 <= 1)
+				return true;
+		}
+	}
+
+	// no collisions detected
+	return false;
+}
 
 // processes key presses
 void keyOperations(void) {
 	if (keyStates[27]) // escape
 		exit(0);
-
+		
 	if (keyStates['w'] && playerCar.acceleration < maxSpeed)
 		playerCar.acceleration += 0.000009f;
+	
 
-	if (keyStates['s'])
+	if (keyStates['s']) 
 		playerCar.acceleration -= 0.000009f;
 
-	if (keyStates['a'])
+
+	if (keyStates['a']) {
 		playerCar.rot += 0.05f;
 
-	if (keyStates['d'])
-		playerCar.rot -= 0.05f;
+		// disable rotation if colliding
+		if (isColliding(playerCar.getCarCorners(), trackPoints)) {
+			playerCar.rot -= 0.05f;
+		}
+	}
 
+	if (keyStates['d']) {
+		playerCar.rot -= 0.05f;
+		
+		// disable rotation if colliding
+		if (isColliding(playerCar.getCarCorners(), trackPoints)) {
+			playerCar.rot += 0.05f;
+		}
+	}
 }
 
 // processes special key presses
@@ -144,33 +231,6 @@ int loadTextures()
 	return true;
 }
 
-// takes in two vectors containing <lines>, checks to see if any of them intersect
-bool isColliding(std::vector<line> a, std::vector<line> b) {
-	float dist1 = 0;
-	float dist2 = 0;
-
-	// loop through every combination of both vectors
-	for (int i = 0; i < a.size(); i++) {
-		for (int j = 0; j < b.size(); j++) {
-
-			// calculate distance to point of intersection
-			float dist1 =  ((b[j].x2 - b[j].x1) * (a[i].y1 - b[j].y1) - (b[j].y2 - b[j].y1) * (a[i].x1 - b[j].x1)) /
-							((b[j].y2 - b[j].y1) * (a[i].x2 - a[i].x1) - (b[j].x2 - b[j].x1) * (a[i].y2 - a[i].y1));
-
-			float dist2 =  ((a[i].x2 - a[i].x1) * (a[i].y1 - b[j].y1) - (a[i].y2 - a[i].y1) * (a[i].x1 - b[j].x1)) /
-							((b[j].y2 - b[j].y1) * (a[i].x2 - a[i].x1) - (b[j].x2 - b[j].x1) * (a[i].y2 - a[i].y1));
-
-			// if dist1 and dist1 are both between 0 and 1, there is a collision
-			if (dist1 >= 0 && dist1 <= 1 && dist2 >= 0 && dist2 <= 1)
-				return true;
-		}
-	}
-
-	// no collisions detected
-	return false;
-}
-
-
 // draws background
 void renderBackground(void) {
 	// enable and bind texture
@@ -196,9 +256,8 @@ void renderBackground(void) {
 }
 
 void renderCars(void) {
-
 	glPushMatrix();
-	
+		
 	// translate to centre of player car, rotate, then translate back
 	glTranslatef(playerCar.pos_x, playerCar.pos_y, 0.0f);
 	glRotatef(playerCar.rot, 0.0, 0.0, 1.0);
@@ -207,9 +266,22 @@ void renderCars(void) {
 	// apply velocity vector
 	playerCar.vel_x = -sin(playerCar.rot * (3.14159265359 / 180));	// get x velocity after converting to rads
 	playerCar.vel_y = cos(playerCar.rot * (3.14159265359 / 180));	// get y velocity after converting to rads
+	
+	// collision handling
+	if (isColliding(playerCar.getCarCorners(), trackPoints)) {
+		// undo car's movement that caused collision
+		playerCar.pos_x -= playerCar.acceleration * playerCar.vel_x; 
+		playerCar.pos_y -= playerCar.acceleration * playerCar.vel_y;
 
-	playerCar.pos_x += playerCar.acceleration * playerCar.vel_x;
-	playerCar.pos_y += playerCar.acceleration * playerCar.vel_y;
+
+		// set acceleration to 0 so next frame doesn't re-cause collision (will cause clipping)
+		playerCar.acceleration = 0;
+	}
+	// if no collision happens, translate car
+	else {
+		playerCar.pos_x += playerCar.acceleration * playerCar.vel_x;
+		playerCar.pos_y += playerCar.acceleration * playerCar.vel_y;
+	}
 
 	// enable and bind texture
 	glEnable(GL_TEXTURE_2D);
@@ -229,10 +301,21 @@ void renderCars(void) {
 	glTexCoord2d(1.0, 0.0);
 	glVertex3f(playerCar.pos_x + (carWidth / 2), playerCar.pos_y - (carLength / 2), 0.0f); // bottom right
 	glEnd();
-
+	
+	
 	glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
 
+	// draw bounding box (toggled by debugMode flag)
+	if (debugMode) {
+		glBegin(GL_LINES);
+		for (int i = 0; i < playerCar.getCarCorners().size(); i++)
+		{
+			glVertex3f(playerCar.getCarCorners()[i].x1, playerCar.getCarCorners()[i].y1, 0.0f);
+			glVertex3f(playerCar.getCarCorners()[i].x2, playerCar.getCarCorners()[i].y2, 0.0f);
+		}
+		glEnd();
+	}
 }
 
 void renderTrack(void) {
